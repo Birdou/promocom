69c179b9909dc83ef786d86bad133177b1d1e9ab
Birdou <felipesena.m@gmail.com>
2021-11-13 15:46:18 -0300

    Logout e cadastro de loja quando autenticado

--- src/main/java/br/ufrn/imd/promocon/config/WebSecurityConfig.java
+++ src/main/java/br/ufrn/imd/promocon/config/WebSecurityConfig.java
@@ -15,35 +15,29 @@ import br.ufrn.imd.promocon.service.MyUserDetailsService;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	MyUserDetailsService detailsService;
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
-			.authorizeRequests()
-				.anyRequest().permitAll()
-				.and()
-			.formLogin()
-				.defaultSuccessUrl("/")
-				.loginPage("/novo-login")
-				.failureUrl("/login")
-				.permitAll();
+		http.csrf().disable().authorizeRequests().anyRequest().permitAll().and().formLogin().defaultSuccessUrl("/")
+				.loginPage("/novo-login").failureUrl("/login").permitAll();
+		http.logout().logoutSuccessUrl("/");
 	}
-	
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Bean
 	public DaoAuthenticationProvider authenticationProvider() {
 		final DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-		
+
 		authProvider.setPasswordEncoder(passwordEncoder());
 		authProvider.setUserDetailsService(detailsService);
-		
+
 		return authProvider;
 	}
 }
